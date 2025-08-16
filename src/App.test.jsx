4689c1f897391ba7d1c0ce21b5842b5ae9c54b2b
CMTLaki <156497392+Charyeokl@users.noreply.github.com>
2025-08-15 22:05:12 -0500

    basic testing

---
+++ src/App.test.jsx
@@ -0,0 +1,7 @@
+﻿import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders Basic Information heading', () => {
+  render(<App />);
+  expect(screen.getByText(/Basic Information/i)).toBeInTheDocument();
+});
